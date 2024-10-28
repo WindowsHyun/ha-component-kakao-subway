6bd9fba6e89cd7d18b39c6913de338043c1dd61a
WindowsHyun <kwon616@gmail.com>
2024-10-28 12:49:56 +0900

    Change: 그룹에서 디바이스 단위로 수정

--- custom_components/kakao_subway/__init__.py
+++ custom_components/kakao_subway/__init__.py
@@ -14,7 +14,6 @@ PLATFORMS = ["sensor"]
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Kakao Subway component."""
-    hass.data.setdefault(DOMAIN, {})
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
@@ -30,24 +29,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
+    # Forward setup to the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 class KakaoSubwayDataUpdateCoordinator(DataUpdateCoordinator):
     """Class to manage fetching Kakao Subway data."""
--- custom_components/kakao_subway/sensor.py
+++ custom_components/kakao_subway/sensor.py
@@ -3,6 +3,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity import DeviceInfo
 
 from .const import DOMAIN, _LOGGER
 
@@ -11,9 +12,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     _LOGGER.debug("Setting up Kakao Subway sensor platform...")
     coordinator = hass.data[DOMAIN][entry.entry_id]
     station_name = entry.data["station_name"]
+    station_id = entry.data["station_id"]
 
     sensors = [
-        sensor_type(coordinator, direction, index, station_name)
+        sensor_type(coordinator, direction, index, station_name, station_id)
         for direction in ["up", "down"]
         for index in [1, 2]
         for sensor_type in [KakaoSubwayDestinationSensor, KakaoSubwayTimeSensor]
@@ -25,12 +27,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 class KakaoSubwayBaseSensor(CoordinatorEntity, SensorEntity):
     """Base class for Kakao Subway sensors."""
 
-    def __init__(self, coordinator, direction, index, station_name, sensor_type):
+    def __init__(self, coordinator, direction, index, station_name, station_id, sensor_type):
         """Initialize the sensor."""
         super().__init__(coordinator)
         self.direction = direction
         self.index = index
         self.station_name = station_name
+        self.station_id = station_id
         self.sensor_type = sensor_type
         self._attr_unique_id = f"{coordinator.station_id}_{direction}_{index}_{sensor_type}"
         _LOGGER.debug("Initialized sensor: %s", self._attr_unique_id)
@@ -39,13 +42,21 @@ class KakaoSubwayBaseSensor(CoordinatorEntity, SensorEntity):
     def name(self):
         """Return the name of the sensor."""
         return f"{self.station_name} {self.direction.title()}bound #{self.index} {self.sensor_type.title()}"
+    
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device information."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.station_id)},
+            name=self.station_name,
+        )
 
 class KakaoSubwayDestinationSensor(KakaoSubwayBaseSensor):
     """Representation of a Kakao Subway destination sensor."""
 
-    def __init__(self, coordinator, direction, index, station_name):
+    def __init__(self, coordinator, direction, index, station_name, station_id):
         """Initialize the sensor."""
-        super().__init__(coordinator, direction, index, station_name, "destination")
+        super().__init__(coordinator, direction, index, station_name, station_id, "destination")
 
     @property
     def state(self):
@@ -62,9 +73,9 @@ class KakaoSubwayDestinationSensor(KakaoSubwayBaseSensor):
 class KakaoSubwayTimeSensor(KakaoSubwayBaseSensor):
     """Representation of a Kakao Subway time sensor."""
 
-    def __init__(self, coordinator, direction, index, station_name):
+    def __init__(self, coordinator, direction, index, station_name, station_id):
         """Initialize the sensor."""
-        super().__init__(coordinator, direction, index, station_name, "time_left")
+        super().__init__(coordinator, direction, index, station_name, station_id, "time_left")
 
     @property
     def state(self):
